a0bdb70a1ff075ca7e0391bc68f57293c236ecdd
Nicholas Rodrigues Lordello <n@lordello.net>
2024-09-26 16:08:19 +0000

    Update Foundry test to more recent nightly version

--- test/foundry/version.sh
+++ test/foundry/version.sh
@@ -3,9 +3,9 @@
 set -e
 source dev-container-features-test-lib
 
-check "forge" bash -c "forge --version | grep '11b08e9'"
-check "cast" bash -c "cast --version | grep '11b08e9'"
-check "anvil" bash -c "anvil --version | grep '11b08e9'"
-check "chisel" bash -c "chisel --version | grep '11b08e9'"
+check "forge" bash -c "forge --version | grep '9a0f66e'"
+check "cast" bash -c "cast --version | grep '9a0f66e'"
+check "anvil" bash -c "anvil --version | grep '9a0f66e'"
+check "chisel" bash -c "chisel --version | grep '9a0f66e'"
 
 reportResults
